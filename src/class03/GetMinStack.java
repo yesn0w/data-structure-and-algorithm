7074b1a102054ee07b2fae58e667f9213e27ff89
yesn0w <yesn0w@outlook.com>
2023-07-27 18:19:28 +0800

    202307271819

--- src/class03/GetMinStack.java
+++ src/class03/GetMinStack.java
@@ -1,4 +1,36 @@
 package class03;
 
+import java.util.Stack;
+
 public class GetMinStack {
+
+    public static class MinStack {
+        private Stack<Integer> normStack;
+        private Stack<Integer> minStack;
+
+        public MinStack() {
+            normStack = new Stack<>();
+            minStack = new Stack<>();
+        }
+
+        public void push(int value) {
+            normStack.push(value);
+            minStack.push(minStack.isEmpty() ? value : (minStack.peek() < value ? minStack.peek() : value));
+        }
+
+        public int pop() {
+            if (normStack.isEmpty()) {
+                throw new RuntimeException("空了，不能再吐了！");
+            }
+            minStack.pop();
+            return normStack.pop();
+        }
+
+        public int getMin() {
+            if (normStack.isEmpty()) {
+                throw new RuntimeException("空的，不能获得最小值！");
+            }
+            return minStack.peek();
+        }
+    }
 }
--- src/class03/Queue2Stack.java
+++ src/class03/Queue2Stack.java
@@ -1,4 +1,60 @@
 package class03;
 
+import sun.awt.image.ImageWatched;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Queue2Stack {
+
+    public static class MyStack {
+        public Queue<Integer> queue;
+        public Queue<Integer> helper;
+
+        public MyStack() {
+            queue = new LinkedList<>();
+            helper = new LinkedList<>();
+        }
+
+        public void push(int value) {
+            queue.offer(value);
+        }
+
+        public int pop() {
+            if (queue.isEmpty()) {
+                throw new RuntimeException("空了，吐不出来了！");
+            }
+            while (queue.size() > 1) {
+                helper.offer(queue.remove());
+            }
+            int res = queue.poll();
+            Queue<Integer> tmp = new LinkedList<>();
+            tmp = queue;
+            queue = helper;
+            helper = tmp;
+            return res;
+        }
+
+        public int peek() {
+            if (queue.isEmpty()) {
+                throw new RuntimeException("空的，获取不了栈顶值！");
+            }
+            while (queue.size() > 1) {
+                helper.offer(queue.remove());
+            }
+            int res = queue.poll();
+            helper.offer(res);
+            Queue<Integer> tmp = new LinkedList<>();
+            tmp = queue;
+            queue = helper;
+            helper = tmp;
+            return res;
+        }
+
+        public boolean isEmpty() {
+            return queue.isEmpty();
+        }
+    }
+
+
 }
