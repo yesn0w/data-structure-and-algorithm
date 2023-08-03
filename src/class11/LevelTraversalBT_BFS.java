255a6b8819b092514fca9c99dd72e52cb23bf8e1
yesn0w <yesn0w@outlook.com>
2023-08-03 11:15:06 +0800

    202308031115

--- src/class11/LevelTraversalBT_BFS.java
+++ src/class11/LevelTraversalBT_BFS.java
@@ -1,4 +1,40 @@
 package class11;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LevelTraversalBT_BFS {
+
+    public class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public void Node(int value) {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+
+    public static void levelTraversalBT(Node head) {
+        if (head == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(head);
+        while (!queue.isEmpty()) {
+            Node cur = queue.remove();
+            System.out.println(cur.value);
+            Node left = cur.left;
+            Node right = cur.right;
+            if (left != null) {
+                queue.add(left);
+            }
+            if (right != null) {
+                queue.add(right);
+            }
+        }
+    }
+
 }
