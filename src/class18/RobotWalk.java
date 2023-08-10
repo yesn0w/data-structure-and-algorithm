66297a68c1fd76953ae3339a37f572a1e37a01fa
yesn0w <yesn0w@outlook.com>
2023-08-10 13:23:35 +0800

    1-202308101323

--- src/class18/RobotWalk.java
+++ src/class18/RobotWalk.java
@@ -11,18 +11,18 @@ public class RobotWalk {
         return process1(N, start, aim, K);
     }
 
-    public static int process1(int N, int cur, int aim, int res) {
-        if (res == 0) {
+    public static int process1(int N, int cur, int aim, int rest) {
+        if (rest == 0) {
             return aim == cur ? 1 : 0;
         }
         if (cur == 1) {
-            return process1(N, 2, aim, res - 1);
+            return process1(N, 2, aim, rest - 1);
         }
         else if (cur == N) {
-            return process1(N, N - 1, aim, res - 1);
+            return process1(N, N - 1, aim, rest - 1);
         }
         else {
-            return process1(N, cur - 1, aim, res - 1) + process1(N, cur + 1, aim, res - 1);
+            return process1(N, cur - 1, aim, rest - 1) + process1(N, cur + 1, aim, rest - 1);
         }
     }
     // 自顶向下的动态规划（傻缓存）
@@ -31,23 +31,48 @@ public class RobotWalk {
             return -1;
         }
         int[][] res = new int[N + 1][K + 1];
+        for (int i = 0; i < N + 1; i++) {
+            for (int j = 0; j < K + 1; j++) {
+                res[i][j] = -1;
+            }
+        }
         process2(N, start, aim, K, res);
-        return res[N][K];
+        return res[aim][K];
     }
     public static int process2(int N, int cur, int aim, int rest, int[][] res) {
-        if (res == 0) {
+        if (rest == 0) {
             return aim == cur ? 1 : 0;
         }
+        if (res[cur][rest] != -1) {
+            return res[cur][rest];
+        }
+        int ans = 0;
         if (cur == 1) {
-            return process1(N, 2, aim, res - 1);
+            ans =  process2(N, 2, aim, rest - 1, res);
         }
         else if (cur == N) {
-            return process1(N, N - 1, aim, res - 1);
+            ans = process2(N, N - 1, aim, rest - 1, res);
         }
         else {
-            return process1(N, cur - 1, aim, res - 1) + process1(N, cur + 1, aim, res - 1);
+            ans = process2(N, cur - 1, aim, rest - 1, res) + process2(N, cur + 1, aim, rest - 1, res);
         }
+        res[cur][rest] = ans;
+        return ans;
     }
     // 严格动态规划
-
+    public static int robotWalk3(int N, int start, int aim, int K) {
+        if ((N < 2 && K > 0) || start < 1 || start > N || aim < 1 || aim > N || K < 1) {
+            return -1;
+        }
+        int[][] res = new int[N + 1][K + 1];
+        res[0][aim] = 0;
+        for (int j = 1; j < K + 1; j++) {
+            res[1][j] = res[2][j - 1];
+            for (int i = 2; j < N; i++) {
+                res[i][j] = res[i - 1][j - 1] + res[i + 1][j - 1];
+            }
+            res[N][j] = res[N - 1][j - 1];
+        }
+        return res[aim][K];
+    }
 }
--- src/class18/TakingCards.java
+++ src/class18/TakingCards.java
@@ -1,4 +1,16 @@
 package class18;
 
 public class TakingCards {
+
+    // 暴力递归
+    // 返回获胜者的最大分数
+    public static int maxPoints(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+        int
+    }
+    // 自顶向下的动态规划（傻缓存）
+    // 严格动态规划
+
 }
